fc3b37dc37fc412e6889df16059e06954d395c79
huguodong <531035580@QQ.COM>
2016-01-08 09:43:17 +0800

    第二次修改

--- Sample/PtrrvGridViewMode.cs
+++ Sample/PtrrvGridViewMode.cs
@@ -19,12 +19,11 @@ namespace Sample
     public class PtrrvGridViewMode : AppCompatActivity, PullToRefreshRecyclerView.PullToRefreshRecyclerView.PagingableListener, SwipeRefreshLayout.IOnRefreshListener
     {
         private PullToRefreshRecyclerView.PullToRefreshRecyclerView mPtrrv;
-        public PtrrvAdapter mAdapter;
-        private static int DEFAULT_ITEM_SIZE = 60;
-        private static int ITEM_SIZE_OFFSET = 40;
+        private MyAdapter mAdapter;
+        private static int DEFAULT_ITEM_SIZE = 100;
         private static int MSG_CODE_REFRESH = 0;
         private static int MSG_CODE_LOADMORE = 1;
-
+        private List<String> list;
         private static int TIME = 1000;
         public Handler mHandler;
         protected override void OnCreate(Bundle savedInstanceState)
@@ -32,6 +31,8 @@ namespace Sample
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.activity_gridview);
             mHandler = new Handler(HandleMessage);
+            list = new List<string>();
+            add();
             FindViews();
             // Create your application here
         }
@@ -39,16 +40,24 @@ namespace Sample
         {
             mPtrrv = FindViewById<PullToRefreshRecyclerView.PullToRefreshRecyclerView>(Resource.Id.ptrrv);
             mPtrrv.SetSwipeEnable(true);//open swipe
-            mPtrrv.SetLayoutManager(new GridLayoutManager(this, 4));
+            mPtrrv.SetLayoutManager(new GridLayoutManager(this, 2));
             mPtrrv.SetPagingableListener(this);
             mPtrrv.SetOnRefreshListener(this);
-            mAdapter = new PtrrvAdapter(this);
-            mAdapter.SetCount(DEFAULT_ITEM_SIZE);
+            mAdapter = new MyAdapter(list, this);
             mPtrrv.SetAdapter(mAdapter);
-            mPtrrv.SetLoadmoreString("12312312312");
             mPtrrv.OnFinishLoading(true, false);
         }
 
+        public void add()
+        {
+            for (int i = 0; i <= 6; i++)
+            {
+                list.Add("Brasil");
+                list.Add("Mexico");
+                list.Add("United States");
+                list.Add("Canada");
+            }
+        }
         public void OnLoadMoreItems()
         {
             mHandler.SendEmptyMessageDelayed(MSG_CODE_LOADMORE, TIME);
@@ -62,51 +71,73 @@ namespace Sample
         {
             if (msg.What == MSG_CODE_REFRESH)
             {
-                mAdapter.SetCount(DEFAULT_ITEM_SIZE);
+                for (int i = 0; i <= 6; i++)
+                { list.Insert(0, "新添的"); }
                 mAdapter.NotifyDataSetChanged();
                 mPtrrv.SetOnRefreshComplete();
                 mPtrrv.OnFinishLoading(true, false);
             }
             else if (msg.What == MSG_CODE_LOADMORE)
             {
-                if (mAdapter.ItemCount == DEFAULT_ITEM_SIZE + ITEM_SIZE_OFFSET)
+                if (mAdapter.ItemCount >= DEFAULT_ITEM_SIZE)
                 {
-                    //over
+                    //加载完毕
                     Toast.MakeText(this, Resource.String.nomoredata, ToastLength.Short).Show();
                     mPtrrv.OnFinishLoading(false, false);
                 }
                 else
                 {
                     Toast.MakeText(this, "正在加载中", ToastLength.Short).Show();
-                    mAdapter.SetCount(DEFAULT_ITEM_SIZE + ITEM_SIZE_OFFSET);
+                    add();
                     mAdapter.NotifyDataSetChanged();
                     mPtrrv.OnFinishLoading(true, false);
                 }
             }
         }
-        public class PtrrvAdapter : PtrrvBaseAdapter<PtrrvAdapter.ViewHolder>
+        public class MyAdapter : RecyclerView.Adapter
         {
-            public PtrrvAdapter(Context context)
-                : base(context)
+            List<string> list;
+            Context context;
+            public MyAdapter(List<string> data, Context context)
             {
-
+                list = data;
+                this.context = context;
             }
+
+            // Create new views (invoked by the layout manager)
             public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
             {
-                View view = mInflater.Inflate(Resource.Layout.ptrrv_item, null);
+                View view = LayoutInflater.From(context).Inflate(Resource.Layout.ptrrv_item, null);
                 return new ViewHolder(view);
             }
-            public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
-            {
 
+            // Replace the contents of a view (invoked by the layout manager)
+            public override void OnBindViewHolder(RecyclerView.ViewHolder viewHolder, int position)
+            {
+                var item = list[position];
+
+                // Replace the contents of the view with that element
+                var holder = viewHolder as ViewHolder;
+                holder.TextView.Text = item;
             }
-            public class ViewHolder : RecyclerView.ViewHolder
-            {
 
-                public ViewHolder(View itemView)
-                    : base(itemView)
+            public override int ItemCount
+            {
+                get
                 {
+                    return list.Count; ;
                 }
+            }
+
+        }
+        public class ViewHolder : RecyclerView.ViewHolder
+        {
+            public TextView TextView { get; set; }
+
+            public ViewHolder(View itemView)
+                : base(itemView)
+            {
+                TextView = itemView.FindViewById<TextView>(Resource.Id.tvContent);
             }
         }
     }
